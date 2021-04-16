3d98606576d432066d758da60ebf71d5494f0e53
ivanurra <ivan.urra.wd@gmail.com>
2021-04-16 14:30:21 +0200

    v-model checkbox

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,49 @@
     placeholder="Write your name"
     v-model="task.name"
   >
+  <br/>
+  <div class="form-check form-check-inline">
+    <input 
+    type="checkbox"
+    id="check-1"
+    class="form-check-input"
+    v-model="task.categories"
+    value="JavaScript"
+    >
+    <label for="check-2" class="form-check-label">JavaScript</label>
+  </div>
+  <div class="form-check form-check-inline">
+    <input 
+    type="checkbox"
+    id="check-1"
+    class="form-check-input"
+    v-model="task.categories"
+    value="NodeJS"
+    >
+    <label for="check-3" class="form-check-label">NodeJS</label>
+  </div>
+  <div class="form-check form-check-inline">
+    <input 
+    type="checkbox"
+    id="check-1"
+    class="form-check-input"
+    v-model="task.categories"
+    value="VueJS"
+    >
+    <label for="check-4" class="form-check-label">VueJS</label>
+  </div>
+  <div class="form-check form-check-inline">
+    <input 
+    type="checkbox"
+    id="check-1"
+    class="form-check-input"
+    v-model="task.categories"
+    value="ReactJS"
+    >
+    <label for="check-1" class="form-check-label">ReactJS</label>
+  </div>
 </form>
+<br/>
 <p>
   {{ task }}
 </p>
@@ -22,7 +64,8 @@ export default {
   data() {
     return {
       task: {
-        name: ''
+        name: '',
+        categories: []
       }
     }
   },
