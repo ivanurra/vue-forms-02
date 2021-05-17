fa003a5742f4fd831c75ee439b91615e07615881
ivanurra <ivan.urra.wd@gmail.com>
2021-05-17 16:40:49 +0200

    v-for x 2

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -15,7 +15,13 @@
       <tr v-for="item in tasks" :key="item.id">
         <th scope="row">{{item.id}}</th>
         <td>{{item.name}}</td>
-        <td>{{item.categories}}</td>
+        <td>
+            <span v-for="(cat, index) in item.categories" :key="index">
+                {{
+                    (item.categories.length === index + 1) ? cat : cat + ', '
+                }}
+            </span>
+        </td>
         <td>{{item.status}}</td>
         <td>{{item.number}}</td>
         <td>Action</td>
